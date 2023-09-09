35d481200ca1bda9cf824e264a98b4ac9fc3834b
Steven Clontz <steven.clontz@gmail.com>
2023-09-09 01:34:42 +0000

    refactor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
 const goFirst:string = "abcdeedcbadcbeebcdaaaaecbddbceecbddbceaaaadcbeebcdabcdeedcbaaadcbeebcdabcdeedcbaaecdbbdceaecdbbdceaabcdeedcbadcbeebcdaaaaaaecbddbceaabceddecbdcebbecdaadcebbecdbceddecbaaecbddbceaaaaaadcebbecdaecdbbdceaabcdeedcbabcdeedcbaaecdbbdceadcebbecdaaaecbddbceabceddecbaaaadcebbecddcebbecdaaaabceddecbaecbddbcea"
-const faces = [
-    [...goFirst.matchAll(/a/g)].map(v=>typeof v.index == "undefined" ? 0 : v.index+1),
-    [...goFirst.matchAll(/b/g)].map(v=>typeof v.index == "undefined" ? 0 : v.index+1),
-    [...goFirst.matchAll(/c/g)].map(v=>typeof v.index == "undefined" ? 0 : v.index+1),
-    [...goFirst.matchAll(/d/g)].map(v=>typeof v.index == "undefined" ? 0 : v.index+1),
-    [...goFirst.matchAll(/e/g)].map(v=>typeof v.index == "undefined" ? 0 : v.index+1),
-]
+const faces = ["a","b","c","d","e"].map(l=>[...goFirst.matchAll(
+    new RegExp(l,"g"))].map(v=>typeof v.index == "undefined" ? 0 : v.index+1)
+)
 const sample = (a:Array<number>) => {
     return a[Math.floor((Math.random()*a.length))]
 }
 const roll = () => {
     return faces.map(face=>sample(face))
 }
-let rolls = ""
+let rolls : number[][] = []
+$: displayRolls =  rolls.map(roll=>roll.toString()).join("\n")
 let rolling = false
 const appendRoll = () => {
     if (rolling) {
-        rolls = rolls + roll().toString() + "\n"
+        rolls = [...rolls, roll()]
     }
 }
 setInterval(appendRoll,500);
 </script>
 
-<textarea disabled rows="10" value={rolls}/>
+<textarea disabled rows="10" value={displayRolls}/>
 
 <button on:click={()=>rolling=!rolling}>
     {#if rolling}
