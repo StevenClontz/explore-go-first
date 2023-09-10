c9bc8ba49e7166140d746d598ad8cd321c540a2e
Steven Clontz <steven.clontz@gmail.com>
2023-09-10 20:52:53 +0000

    sort fairness check

--- src/lib/fairnessChecks.ts
+++ src/lib/fairnessChecks.ts
@@ -5,21 +5,30 @@ interface fairnessCheck {
 // Based upon http://gofirstdice.ericharshbarger.org/doku.php?id=fast_perm_check
 // Locks up with sufficiently many letters in code...
 export const computePermCheck = (code:string) : fairnessCheck => {
-    let perms:string[] = [""]
     let permCheck:fairnessCheck = {"": 1}
     code.split("").forEach(l=>{
-        perms.forEach(perm=>{
+        Object.keys(permCheck).forEach(perm=>{
             if (perm.indexOf(l)==-1) {
-                if (perms.includes(perm+l)) {
+                if (Object.keys(permCheck).includes(perm+l)) {
                     permCheck[perm+l] += permCheck[perm]
                 } else {
-                    perms.push(perm+l)
                     permCheck[perm+l] = permCheck[perm] 
                 }
             }
         })
     })
-    return permCheck
+    const perms = Object.keys(permCheck).sort((a,b)=>{
+        if (a.length==b.length) {
+            if (a<b) {
+                return -1
+            }
+            return 1
+        }
+        return a.length-b.length
+    })
+    let sortedPermCheck:fairnessCheck = {}
+    perms.forEach(perm=>sortedPermCheck[perm]=permCheck[perm])
+    return sortedPermCheck
 }
 
 // ,
