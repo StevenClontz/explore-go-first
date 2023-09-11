8a9bfcc0daf6e9407f4a7b0e061c615e9bd3e646
Steven Clontz <steven.clontz@gmail.com>
2023-09-11 02:25:34 +0000

    more stable chart yAxis resizing

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -4,6 +4,8 @@
     import type { Dice } from '$lib/dice'
     let chartCanvas:HTMLCanvasElement
     let chart:Chart
+    let yAxisMin = 0
+    let yAxisMax = 100
     export let dice:Dice
     const updateChart = (c:Chart,dice:Dice) => {
         c.data.labels = dice.names
@@ -11,15 +13,23 @@
             c.data.datasets[rank].data = dice.percentages[rank]
         })
         if (c.options.scales && c.options.scales.y) {
-            c.options.scales.y.min = Math.min(
+            const newMin = Math.min(
                 Math.floor(dice.minPercentage()),
                 Math.max(0,Math.floor(dice.minPercentage()-0.5*dice.percentageRange()))
             )
-            c.options.scales.y.max = Math.max(
+            if (newMin > yAxisMin || newMin < yAxisMin-1) {
+                yAxisMin = newMin
+                c.options.scales.y.min = yAxisMin
+            }
+            const newMax = Math.max(
                 Math.ceil(dice.maxPercentage()),
                 Math.min(100,Math.ceil(dice.maxPercentage()+0.5*dice.percentageRange())),
                 10
             )
+            if (newMax < yAxisMax || newMax > yAxisMax+1) {
+                yAxisMax = newMax
+                c.options.scales.y.max = yAxisMax
+            }
         }
         c.update()
     }
@@ -40,8 +50,8 @@
                 options: {
                     scales: {
                         y: {
-                            min: 0,
-                            max: 100,
+                            min: yAxisMin,
+                            max: yAxisMax,
                         }
                     }
                 }
