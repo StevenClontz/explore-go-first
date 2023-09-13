77b03d139581a666053da45f947f603d32f0fe72
Steven Clontz <steven.clontz@gmail.com>
2023-09-13 00:00:54 +0000

    add new homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+import { Dice } from "$lib/dice"
+import exampleDiceJson from "$lib/exampleDice.json?raw"
+import { page } from "$app/stores";
+interface exampleDiceI {
+    name: string
+    code: string
+}
+const exampleDice : exampleDiceI[] = JSON.parse(exampleDiceJson)
+let code:string = exampleDice[0].code
+if ($page.url.hash) {
+    code = $page.url.hash.slice(1)
+}
+let codeSelection:string|undefined
+let dice = new Dice(code)
+const updateCodeSelection = () => {
+    if (codeSelection) {
+        code = codeSelection
+    }
+}
+$: if (code) {
+    if (exampleDice.map(d=>d.code).includes(code)) {
+        codeSelection = code
+    } else {
+        codeSelection = undefined
+    }
+}
+</script>
+
+<svelte:head>
+    <title>Explore Go-First Dice</title>
+</svelte:head>
+
+<div>
+    <select style="width:100%" bind:value={codeSelection} on:change={updateCodeSelection}>
+        <option value={undefined}>
+            Custom die
+        </option>
+        {#each exampleDice as exampleDie}
+            <option value={exampleDie.code}>
+                {exampleDie.name}
+            </option>
+        {/each}
+    </select>
+</div>
+
+<div>
+    <input style="width:100%" bind:value={code}/>
+</div>
+
+
+<p>
+    <a href="/dashboard">Dashboard</a>
+</p>
+
+<p>
+    <a href="https://github.com/StevenClontz/explore-go-first">GitHub</a> |
+    <a href="http://gofirstdice.ericharshbarger.org">Learn more</a>
+</p>
